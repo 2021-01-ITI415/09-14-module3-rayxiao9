69d8a607220e335e4b65374212151f5c98ebc68c
rayxiao9 <77815877+rayxiao9@users.noreply.github.com>
2021-04-22 15:51:51 -0400

    AudioMixer Snapshot transitions

--- Assets/_Labs/05-Audio/Common/Scripts/PlayerAudio.cs
+++ Assets/_Labs/05-Audio/Common/Scripts/PlayerAudio.cs
@@ -1,22 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class PlayerAudio : MonoBehaviour
 {
     public AudioClip splashSound;
 
     public AudioSource audioS;
+
+    public AudioMixerSnapshot idleSnapshot;
+    public AudioMixerSnapshot auxInSnapshot;
     
     private void OnTriggerEnter(Collider other) {
-        if (other.CompareTag("Water")){
+        if (other.CompareTag("Water")) {
             audioS.PlayOneShot(splashSound);
+        }
+        if (other.CompareTag("EnemyZone")) {
+            auxInSnapshot.TransitionTo(0.5f);
         }
     }
 
     private void OnTriggerExit(Collider other) {
-        if (other.CompareTag("Water")){
+        if (other.CompareTag("Water")) {
             audioS.PlayOneShot(splashSound);
+        }
+        if (other.CompareTag("EnemyZone")) {
+            idleSnapshot.TransitionTo(0.5f);
         }
     }
 }
