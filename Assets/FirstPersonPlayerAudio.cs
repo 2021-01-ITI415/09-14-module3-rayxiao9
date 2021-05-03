11b16facabe645a7ecbdce8ff0797e09707541b8
rayxiao9 <77815877+rayxiao9@users.noreply.github.com>
2021-05-03 19:11:52 -0400

    Trees and Details

--- Assets/FirstPersonPlayerAudio.cs
+++ Assets/FirstPersonPlayerAudio.cs
@@ -1,16 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class FirstPersonPlayerAudio : MonoBehaviour
 {
     public AudioClip splashSound;
 
     public AudioSource audioS;
+
+    public AudioMixerSnapshot ambIdleSnapshot;
+    public AudioMixerSnapshot ambInSnapshot;
     
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Water")) {
             audioS.PlayOneShot(splashSound);
+        }
+        if (other.CompareTag("Ambience")) {
+            ambInSnapshot.TransitionTo(0.5f);
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.CompareTag("Ambience")) {
+            ambIdleSnapshot.TransitionTo(0.5f);
         }
     }
 }
