788734f2559e7e0a0d245c584ca8a8022154d7fc
rayxiao9 <77815877+rayxiao9@users.noreply.github.com>
2021-05-03 18:19:59 -0400

    Water splash audio

---
+++ Assets/FirstPersonPlayerAudio.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonPlayerAudio : MonoBehaviour
+{
+    public AudioClip splashSound;
+
+    public AudioSource audioS;
+    
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Water")) {
+            audioS.PlayOneShot(splashSound);
+        }
+    }
+}
