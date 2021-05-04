8455a7139a93fde6b5a2e79182066f1de389ddcc
rayxiao9 <77815877+rayxiao9@users.noreply.github.com>
2021-05-03 20:46:45 -0400

    Finish audio and UI text

--- Assets/FirstPersonPlayerAudio.cs
+++ Assets/FirstPersonPlayerAudio.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Audio;
 
 public class FirstPersonPlayerAudio : MonoBehaviour
@@ -15,6 +16,10 @@ public class FirstPersonPlayerAudio : MonoBehaviour
 
     public AudioMixerSnapshot ambIdleSnapshot;
     public AudioMixerSnapshot ambInSnapshot;
+    public AudioMixerSnapshot finishedSnapshot;
+
+    private int tokens = 0;
+    public Text tokenCounter, finishedText, collectAllText;
     
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Water")) {
@@ -27,9 +32,25 @@ public class FirstPersonPlayerAudio : MonoBehaviour
             audioS.PlayOneShot(pickUpSound);
             ambIdleSnapshot.TransitionTo(0.5f);
             Destroy(other.gameObject);
+            tokens++;
+            tokenCounter.text = "Tokens Collected " + tokens;
+            if (tokens == 29) {
+                tokenCounter.gameObject.SetActive(false);
+                collectAllText.gameObject.SetActive(true);
+                finishedSnapshot.TransitionTo(0.5f);
+                audioS.PlayOneShot(victorySound);
+            }
         }
         if (other.CompareTag("Fin")) {
+            finishedSnapshot.TransitionTo(0.5f);
             audioS.PlayOneShot(victorySound);
+            tokenCounter.gameObject.SetActive(false);
+            if (tokens == 1) {
+                finishedText.text = "You Finished with\n" + tokens + "\nToken!";
+            } else {
+                finishedText.text = "You Finished with\n" + tokens + "\nTokens!";
+            }
+            finishedText.gameObject.SetActive(true);
         }
     }
 
@@ -37,5 +58,29 @@ public class FirstPersonPlayerAudio : MonoBehaviour
         if (other.CompareTag("Ambience")) {
             ambIdleSnapshot.TransitionTo(0.5f);
         }
+    }
+
+    void Awake() {
+        SetUpUITexts();
+    }
+
+    void SetUpUITexts() {
+        GameObject go = GameObject.Find("Tokens");
+        if (go != null) {
+            tokenCounter = go.GetComponent<Text>();
+        }
+
+        go = GameObject.Find("Finish Text");
+        if (go != null) {
+            finishedText = go.GetComponent<Text>();
+        }
+
+        go = GameObject.Find("Collected All Text");
+        if (go != null) {
+            collectAllText = go.GetComponent<Text>();
+        }
+
+        finishedText.gameObject.SetActive(false);
+        collectAllText.gameObject.SetActive(false);
     }
 }
